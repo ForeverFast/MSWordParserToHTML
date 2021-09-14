83f91bf02ec3e8f1da054e765525db4f3c194a0a
FordRelak <FordRelak.4050@yandex.ru>
2021-09-14 16:37:57 +0300

    Revert "123"
    This reverts commit 0c9b27cc62c6419e1dab85c3c06988fec67ac090.

--- MSWordParserToHTML/Commands/CommandBase/RelayCommand.cs
+++ MSWordParserToHTML/Commands/CommandBase/RelayCommand.cs
@@ -4,9 +4,10 @@ using System.Windows.Input;
 
 namespace MSWordParserToHTML.Commands
 {
+    #region Делегаты для методов WPF команд
     public delegate void ExecuteHandler(object parameter);
     public delegate bool CanExecuteHandler(object parameter);
-    
+    #endregion
 
     #region Класс команд - RelayCommand
     /// <summary>Класс реализующий интерфейс ICommand для создания WPF команд</summary>
